3c4e338b290c2616f8a6cf322f07c8df85e90ed9
Ishan <ishanbaliyan@gmail.com>
2021-05-31 00:06:42 -0400

    Added Construction Message

--- official_full_ishan_baliyan_personal_website/src/App.js
+++ official_full_ishan_baliyan_personal_website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          🔨 This is site is currently under construction! Check back later for updates ...
         </p>
         <a
           className="App-link"
