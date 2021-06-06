bd1467ee09b2e5828bc2339621e94a8eb8ad2b21
Ishan <ishanbaliyan@gmail.com>
2021-06-06 01:13:46 -0400

    Added Project Information section

---
+++ official_full_ishan_baliyan_website/src/components/portfolioList/PortfolioList.js
@@ -0,0 +1,13 @@
+import "./portfolioList.scss"
+
+export default function PortfolioList() {
+    return (
+        <div className="portfolioList">
+            <li className="active">Featured</li>
+                <li className>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+        </div>
+    )
+}
--- official_full_ishan_baliyan_website/src/components/projects/Projects.js
+++ official_full_ishan_baliyan_website/src/components/projects/Projects.js
@@ -1,8 +1,38 @@
 import "./projects.scss"
 
 export default function Projects() {
+
     return (
         <div className="projects" id="projects">
+            <h1>Projects</h1>
+            <ul>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/portfolio_donkey_kong_example.png" alt=""/>
+                    <h3>Donkey Kong Java Game</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/portfolio_resella_logo.png" alt=""/>
+                    <h3>Resella Java Webscraper</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/portfolio_high_low_game.png" alt=""/>
+                    <h3>High Low Chance Java Game</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/portfolio_gear_one.jpeg" alt=""/>
+                    <h3>In progress...</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/portfolio_gear_two.jpeg" alt=""/>
+                    <h3>In progress...</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/portfolio_gear-three.jpeg" alt=""/>
+                    <h3>In Progress...</h3>
+                </div>
+            </div>
         </div>
     )
 }
